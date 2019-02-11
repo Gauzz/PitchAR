09af6bb1d997e2ed24a36b3adcc3969febf2368b
Gauzz <gauravrozatkar1@gmail.com>
2019-02-11 15:45:04 +0530

    arjs integration updated

--- tests/build/Experience.js
+++ tests/build/Experience.js
@@ -1,6 +1,6 @@
 var start="<html><head><script src='https://aframe.io/releases/0.8.0/aframe.min.js'></script><script src='https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js'></script></head>"
 +"<body style='margin : 0px; overflow: hidden;'>"+
-    "<a-scene arjs='sourceType: webcam;'>"+"<a-marker-camera preset='hiro'>";
+    "<a-scene arjs='sourceType: webcam;debugUIEnabled: false;'>"+"<a-marker-camera preset='hiro'>";
 var mid=" ";
 var end="</a-marker-camera></a-scene></body></html>";
 

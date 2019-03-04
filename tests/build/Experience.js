126b5d8288e686322aa059c7ee2bd1f002ad0418
Gauzz <gauravrozatkar1@gmail.com>
2019-03-04 15:07:02 +0530

    Changes in CTA,Image,Video,Audio

--- tests/build/Experience.js
+++ tests/build/Experience.js
@@ -1,18 +1,24 @@
-var start="<html><head><script src='https://aframe.io/releases/0.8.0/aframe.min.js'></script><script src='https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js'></script></head>"
-+"<body style='margin : 0px; overflow: hidden;'>"+
-    "<a-scene arjs='sourceType: webcam;debugUIEnabled: false;'>"+"<a-marker-camera preset='hiro'>";
-var mid=" ";
-var end="</a-marker-camera></a-scene></body></html>";
+var start1="<html><head><script src='https://aframe.io/releases/0.8.0/aframe.min.js'></script><script src='https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js'></script>"+"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>"+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script></head>"+"<body style='margin : 0px; overflow: hidden;'>"+"<a-scene arjs='sourceType: webcam;debugUIEnabled: false;'>"+"<a-marker-camera preset='hiro'>";
+var mid3d=" ";
+var mid2d="</a-marker-camera></a-scene>";
+var end="<script>function playaud(e){var x= document.getElementById(e.dataset.source);x.play();}</script></body></html>";
+var file;
+
+
 
 function readFile(){
     showEntities();
-    console.log(start+mid+end);
+    console.log(start1+mid3d+mid2d+end);
+    var file=start1+mid3d+mid2d+end;
+    return file;
 }
 
 function showEntities(){
 var sceneEl = document.querySelector('#perswin');    
 var els = sceneEl.querySelectorAll('.exp');
-mid="";
+var els2 = document.querySelectorAll('.exp2');
+mid3d="";
+var temp="";
 for (var i = 0; i < els.length; i++) {
   var rx,ry,rz,sx,sy,sz;
   rx=els[i].object3D.rotation.x;
@@ -25,7 +31,19 @@ for (var i = 0; i < els.length; i++) {
   els[i].setAttribute('scale' , sx+' '+sy+' '+sz);
   els[i].flushToDOM(true);
   els[i].components.rotation.flushToDOM();
-  mid+=els[i].outerHTML;
+  mid3d+=els[i].outerHTML;
 }
-return mid;
+for (var j = 0; j < els2.length; j++) {
+  temp += els2[j].outerHTML;
+  console.log(els2[j]);
 }
+mid2d += temp;
+return mid3d+mid2d;
+}
+
+function shareexp(e){
+  var exp = readFile();
+  var exptxt= document.getElementById('exptxt');
+  exptxt.value = exp;
+  console.log(exp);
+  }

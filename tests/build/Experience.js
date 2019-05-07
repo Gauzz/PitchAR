06d7ee0ae50348c63c5e351ab70d0f60b89cc5e4
Gauzz <gauravrozatkar1@gmail.com>
2019-05-07 18:41:33 +0530

    complex modules integrated

--- tests/build/Experience.js
+++ tests/build/Experience.js
@@ -1,7 +1,7 @@
 var start1 =
 	"<html><head><script src='https://aframe.io/releases/0.9.0/aframe.min.js'></script><script src='https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js'></script>" +
 	"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>" +
-	"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script></head>" +
+	"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script></head>" +
 	"<body style='margin : 0px; overflow: hidden;'>" +
 	"<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam;debugUIEnabled: false;'>";
 var marker = "<a-marker-camera preset='hiro'>";

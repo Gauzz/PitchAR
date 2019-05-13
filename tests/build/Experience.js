6e42ecc1293a8273cbe8cd159fb6ab074458c165
agrawalvivek2017 <agrawal.vivek2017@gmail.com>
2019-05-13 14:12:46 +0530

    UPDATES

--- tests/build/Experience.js
+++ tests/build/Experience.js
@@ -1,6 +1,8 @@
 var start1 =
-	"<html><head><script src='https://aframe.io/releases/0.9.0/aframe.min.js'></script><script src='https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js'></script>" +
-	"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>" +
+	"<html><head><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>" +
+	"<script src='https://aframe.io/releases/0.9.0/aframe.min.js'></script><script src='https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js'></script>" +
+	"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>" + 
+	"<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>" +
 	"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script></head>" +
 	"<body style='margin : 0px; overflow: hidden;'>" +
 	"<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam;debugUIEnabled: false;'>";
@@ -13,7 +15,7 @@ var mid2d = '</a-marker-camera></a-scene>';
 
 var end1="<div id='splashscreen' style='background-color: white; height:100% ; width:100%; position:absolute; top:0px; left:0px; z-index:4;'>"+
 "<img src='"+linkmarker+"' width='40%' style='margin: 10px;float:left;'>"+
-"<a href='#' style='position: absolute;top: 0px;margin: 10px;text-decoration: none;color:#4846ae;'>Share Marker</a>"+
+"<a style='position: absolute;top: 0px;margin: 10px;text-decoration: none;color:#4846ae;'>Share Marker</a>"+
 "<a onclick='facebook()' style='position: absolute;top: 24px;margin: 10px;text-decoration: none;color: #505050; cursor: pointer;'>Facebook</a>"+
 "<a onclick='whatsapp()' data-action='share/whatsapp/share' style='position: absolute;top: 48px;margin: 10px;text-decoration: none;color:#505050; cursor: pointer;'>Whatsapp</a>"+
 "<a onclick='urll()' style='position: absolute;top: 72px;margin: 10px;text-decoration: none;color:#505050; cursor: pointer;'> URL</a>"+
@@ -55,6 +57,7 @@ function showEntities() {
 	}
 	mid2d += temp;
 	return mid3d + mid2d;
+
 }
 
 function sharelnk(e) {
@@ -205,14 +208,14 @@ function editExperience() {
 			var scene = document.getElementById('perswin');
 			dom.innerHTML = data.Data[0].experience;
 			var els = dom.querySelectorAll('.exp');
-			var els2 = document.querySelectorAll('.exp2');
+			var els2 = dom.querySelectorAll('.exp2');
 			console.log(els2);
 			for (var i = 0; i < els.length; i++) {
 				scene.appendChild(els[i]);
 			}
 			for (var i = 0; i < els2.length; i++) {
 				document.getElementById('d2').appendChild(els2[i]);
-				console.log(els2[i]);
+				console.log(dom);
 			}
 		}
 	});
